f368312500259011df4ae2cf0b27bbc1b41aedd6
Viviane Romano <viviane.romano@gx2.com.br>
2024-07-04 18:22:53 -0400

    fix: remove file cache

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
-import dotenv from 'dotenv';
 
-dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslintPlugin()],
